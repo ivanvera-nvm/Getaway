7a1a67e29d06f3e98e9998db040a41c80e654908
ivanvera-nvm <veraivandamian@gmail.com>
2021-03-09 20:05:34 -0300

    react routes updated

---
+++ front/src/components/views/error/Error.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>404 NOT FOUND</h1>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy5cGaTZbfnQlzL89DiMvtB9LNnlb7vSTybA&usqp=CAU"
+        alt="error"
+      />
+    </div>
+  );
+};
+
+export default Error;
---
+++ front/src/components/views/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h3 style={{ color: "green" }}>SIDEBAR</h3>
+    </div>
+  );
+};
+
+export default Sidebar;
