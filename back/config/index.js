038bc03ba9bb975c06caf80e3d3d681182aa3bc9
Paula Dayup <paudayup@gmail.com>
2021-03-10 10:57:46 -0300

    modelos ok

--- back/config/index.js
+++ back/config/index.js
@@ -1,10 +1,4 @@
-/* const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://localhost:5432/getaway", {
-  logging: false,
-});
-
-module.exports = db; */
 const S = require('sequelize') 
 const db = new S('postgres://postgres@localhost/getaway', {
     logging: false
--- back/index.js
+++ back/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
-const db = require("./config/index")
 
+/* const db = require("./config/index") */
+
+const { db } = require("./models/index")
 
 const PORT = 3080;
 
@@ -10,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // {force : true}
-db.sync({force: false})
+db.sync()
 .then(() => {
   app.listen(
     PORT, () => {
@@ -20,12 +22,3 @@ db.sync({force: false})
 })
 .catch(err => console.log(err))
 
-/* 
-db.sync()
-  .then(() => {
-    console.log("sincronizado con la base de datos");
-    app.listen(3001, () => {
-      console.log("server encendido en puerto 3001");
-    });
-  })
-  .catch(console.error); */
