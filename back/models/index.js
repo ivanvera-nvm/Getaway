0746d26e19906fdfe2a5865eebf7c41b15f6e153
Julieta Puig <jpuigperalta@gmail.com>
2021-03-11 13:00:55 -0300

    routesCart actualizando

--- back/models/index.js
+++ back/models/index.js
@@ -7,7 +7,6 @@ const Product= require("./Product")
 const Review = require("./Review")
 const Order = require("./Order")
 const Category = require("./Category")
-const OrderItem = require("./OrderItem")
 
 
 //RELACIONES
@@ -16,13 +15,13 @@ User.hasMany(Review) //userId en Review
 User.hasMany(Order) //userId en Order
 
 Order.belongsTo(User) //userId en Cart
-Order.hasMany(OrderItem) //orderId
+// Order.hasMany(OrderItem) //orderId
 Order.belongsTo(Cart) //cartId
 
 
 Cart.hasOne(Order)
 
-Product.hasMany(OrderItem)
+Product.hasMany(Order)
 Product.hasMany(Review) //productId en Review
 Product.belongsToMany(Review, {through: "product_reviews"})
 
