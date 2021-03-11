b3230852803fb0afa90c2d5f54b7bdf2a3c07d28
ivanvera-nvm <veraivandamian@gmail.com>
2021-03-11 13:46:15 -0300

    updated register and login

--- back/index.js
+++ back/index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
+var cors = require('cors')
 
 const { db } = require("./models/index")
 
 const PORT = 3080;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 app.use("/api", routes)
 
 // {force : true}
-db.sync({force : false})
+db.sync(/* {force : true} */)
 .then(() => {
   app.listen(
     PORT, () => {
--- front/src/components/views/login/Login.jsx
+++ front/src/components/views/login/Login.jsx
@@ -1,14 +1,140 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+
+import { useDispatch } from "react-redux";
+
+import axios from "axios";
+
+import { setUser } from "../../state/user";
+
+import useStyles from "./style";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
 
 const Login = () => {
+  const classes = useStyles();
 
+  const dispatch = useDispatch();
+
+  const getEmail = (e) => {
+    console.log("EMAIL ====>", e.target);
+  };
+
+  const getPass = (e) => {
+    console.log("asdasdas");
+    console.log("PASSWORD ====>", e.target);
+  };
+
+  const sendLoginRequest = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3080/api/users/login", {
+        email: "veraivandamian@gmail.com",
+        password: "123",
+      })
+      .then((res) => res.data)
+      .then((user) => {
+        dispatch(setUser(user));
+      })
+      .catch((err) => alert("hubo un error", err));
+  };
 
   return (
     <div>
-      <h1>LOGIN</h1>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form
+              className={classes.form}
+              noValidate
+              onTo
+              onSubmit={sendLoginRequest}
+            >
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Box mt={5}>
+                <Copyright />
+              </Box>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 };
-
 
 export default Login;
