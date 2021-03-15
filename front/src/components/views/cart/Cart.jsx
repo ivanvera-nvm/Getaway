f53714b29632e86084acb9adc39eda13888782aa
ivanvera-nvm <veraivandamian@gmail.com>
2021-03-15 10:48:16 -0300

    more updates

--- front/src/components/views/cart/Cart.jsx
+++ front/src/components/views/cart/Cart.jsx
@@ -14,6 +14,7 @@ import { useDispatch, useSelector } from "react-redux";
 // Components
 
 const Cart = () => {
+
   const dispatch = useDispatch();
   const classes = useStyles();
 
@@ -66,6 +67,7 @@ const Cart = () => {
       productsFilter(products, order.id, order.productQuantity, i)
     );
 
+    
   return (
     <div>
       {["right"].map((anchor) => (
