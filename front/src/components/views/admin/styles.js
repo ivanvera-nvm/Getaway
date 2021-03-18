517f49bb4f6b3b1c61d2ca04543cef1663bf7f7f
martinagui <martin.agui@gmail.com>
2021-03-18 16:36:13 -0300

    jueves

---
+++ front/src/components/views/admin/styles.js
@@ -0,0 +1,41 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  adminPanel: {
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: "35px",
+    fontSize: "25px",
+  },
+  links: {
+    backgroundColor: "#f57c00",
+    width: "380px",
+    height: "120px",
+    marginTop: "8px",
+    marginBottom: "8px",
+    marginLeft: "8px",
+    marginRight: "8px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textDecoration: "none",
+    textTransform: "uppercase",
+    color: "white",
+    borderRadius: "8px",
+
+    "&:hover": {
+      fontSize: "17px",
+    },
+    "&:active": {
+      color: "gray",
+    },
+  },
+}));
+
+export default useStyles;
--- front/src/components/views/cart/Cart.jsx
+++ front/src/components/views/cart/Cart.jsx
@@ -8,6 +8,7 @@ import {
   List,
 } from "@material-ui/core";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Box from "@material-ui/core/Box";
 
 import { setUserOrders } from "../../../state/orders";
 import { useDispatch, useSelector } from "react-redux";
@@ -26,10 +27,7 @@ const Cart = () => {
   const userOrders = useSelector((state) => state.userOrders);
   const user = useSelector((state) => state.user);
 
-
   const userId = user.user.id;
-
-
 
   useEffect(() => {
     axios
@@ -41,9 +39,6 @@ const Cart = () => {
     right: false,
   });
 
- 
-
-
   const toggleDrawer = (anchor, open) => (event) => {
     if (
       event.type === "keydown" &&
@@ -53,7 +48,6 @@ const Cart = () => {
     }
     setState({ ...state, [anchor]: open });
   };
-
 
   return (
     <div>
@@ -83,13 +77,15 @@ const Cart = () => {
                   </ListItem>
                 </div>
               ) : (
-                <div>
-                  <List>
+                <div className={classes.drawer}>
+                  <List className={classes.mainList}>
                     <ListItem button key={"FULLFILLED"}>
-                      <ListItemText primary={`${user.user.name}'s cart`} />
+                      <Box
+                        className={classes.name}
+                      >{`${user.user.name}'s cart`}</Box>
                     </ListItem>
                   </List>
-                  <List>
+                  <List className={classes.componentList}>
                     {userOrders.map((order) => {
                       return (
                         <ListItem button key={"FULLFILLED"}>
@@ -104,13 +100,14 @@ const Cart = () => {
               )}
             </div>
             <List>
-              <ListItem button key={"FULLFILLED"}>
+              <ListItem className={classes.button} button key={"FULLFILLED"}>
                 <NavLink
+                  className={classes.purchaseBtn}
                   to={{
                     pathname: "/cartDetails",
                   }}
                 >
-                  <ListItemText primary={"Go to detail"} />
+                  Go to detail
                 </NavLink>
               </ListItem>
             </List>
