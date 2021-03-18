77a58e88ec1b674ecc8c56be175bb73f7390cca8
unknown <mariano.julian@hotmail.com>
2021-03-18 09:35:43 -0300

    rutas

--- back/routes/admin.js
+++ back/routes/admin.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/users");
+const ProductController = require("")
 const checkJWT = require("../utils/checkJwt");
 
 router.get("/", checkJWT, UserController.allUsers);
-
-router.get("/" , checkJWT, UserController.allUsers)
 router.get("/:id", checkJWT, UserController.findById)
-router.get("/access", UserController.checkAccess)
-
-router.put("/access/:id", UserController.checkAccess);
-
-router.delete("/:id", UserController.deleteUser);
+router.get("/access",checkJWT, UserController.checkAccess)
+router.put("/access/:id",checkJWT, UserController.checkAccess);
+router.delete("/:id",checkJWT, UserController.deleteUser);
+router.get("/products", ProductController.findAll);
 
 module.exports = router;
